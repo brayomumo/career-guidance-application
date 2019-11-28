d9385dd738944b201d256af5544cde3de8350bb3
Brian <brayomumo5@gmail.com>
2019-11-28 16:50:47 +0300

    Main application

---
+++ application/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class allcourses(models.Model):
+    course_id = models.IntegerField()
+    pro_code = models.IntegerField()
+    university = models.CharField(max_length=200)
+    course_name = models.CharField(max_length=200)
+    cp = models.FloatField()
+    cutoffpoints_2014= models.FloatField()
+    cutoffpoints_2015_cp= models.FloatField()
+    cutoffpoints_2016_cp= models.FloatField()
+    cutoffpoints_2017_cp= models.FloatField()
+    cutoffpoints_2018_cp= models.FloatField()
+    course_id = models.IntegerField()
