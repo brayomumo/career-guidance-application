46ac9e10666bd4db8aaf29a3c2a7095983aafc0b
Brian <brayomumo5@gmail.com>
2019-12-01 23:10:34 +0300

    retrieve database

--- application/views.py
+++ application/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import allcourses
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    courses = allcourses.objects.all()
+    cutoff = 40.028
+    cutoff = int(cutoff)
+    cutoffD = cutoff + 0.999
+    print(cutoffD)
+    print(cutoff)
+    course = allcourses.objects.filter(cutoffpoints_2018_cp__range=(cutoff,cutoffD))
+    for i in course:
+        print(i.course_name)
+    print("Index")
+
+    return render(request, "base.html", {"courses":courses})
+
+def getcourse(request,cutoff):
+    cutoff = int(cutoff)
+    cutoffD = cutoff + 0.999
+    print(cutoff)  
+    course = allcourses.objects.filter(cutoffpoints_2018_cp__range=(cutoff,cutoffD))
+    return render(request, "base.html", {"course":course})
